6b3eeae7f4ae4914a206e25ba9eff05ea291db4a
Ariel I. Diaz <arielivandiaz@gmail.com>
2020-02-04 00:37:03 -0300

    USART_example

--- 1_Blink/src/main.h
+++ 1_Blink/src/main.h
@@ -3,8 +3,8 @@
 
 #include "stm32f4xx_hal.h"
 
-#define LED_PIN                                GPIO_PIN_13
+#define LED_PIN                                GPIO_PIN_14
 #define LED_GPIO_PORT                          GPIOD
-#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOD_CLK_ENABLE()
 
 #endif // MAIN_H
---
+++ 2_USART/src/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#include "tm_stm32_usart.h"
+
+#include "stm32f4xx_hal.h"
+
+#define LED_PIN_1                              GPIO_PIN_12
+#define LED_PIN_2                              GPIO_PIN_13
+#define LED_PIN_3                              GPIO_PIN_14
+#define LED_PIN_4                              GPIO_PIN_15
+#define LED_GPIO_PORT                          GPIOD
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOD_CLK_ENABLE()
+
+#endif // MAIN_H
